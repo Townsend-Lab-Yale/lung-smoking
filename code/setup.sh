a0af559b8d93275ce5bdae3abe4039d0cc4b16ee
Jorge A. Alfaro Murillo <jorge.alfaro-murillo@yale.edu>
2021-07-07 16:19:00 -0400

    setup.sh: Download and extract cBioPortal data

--- code/setup.sh
+++ code/setup.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-# Rscript cBioPortal_API.R
+cd ../data
+
+## cBioPortal data
+URL="https://cbioportal-datahub.s3.amazonaws.com/"
+EXTENSION=".tar.gz"
+
+declare -a DATASETS=("luad_broad"
+                     "lung_msk_2017"
+                     "luad_tsp"
+                     "luad_oncosg_2020"
+                     "luad_mskcc_2015"
+                     "nsclc_pd1_msk_2018"
+                     "nsclc_tracerx_2017")
+
+for DATASET in ${DATASETS[@]}; do
+   curl "${URL}${DATASET}${EXTENSION}" | gunzip -dc | tar -xf -
+done
+
 ## TCGA data is not available in cBioPortal
-curl --output-dir '../data/' -o 'luad_tcga.gz' 'https://api.gdc.cancer.gov/data/0458c57f-316c-4a7c-9294-ccd11c97c2f9'
-gunzip -c '../data/luad_tcga.gz' > '../data/tsp.luad.maf.txt'
-rm '../data/luad_tcga.gz'
+# curl --output-dir '../data/' -o 'luad_tcga.gz' 'https://api.gdc.cancer.gov/data/0458c57f-316c-4a7c-9294-ccd11c97c2f9'
+# gunzip -c '../data/luad_tcga.gz' > '../data/tsp.luad.maf.txt'
+# rm '../data/luad_tcga.gz'
