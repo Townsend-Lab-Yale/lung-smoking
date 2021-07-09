b94b0821d86f7aa45f8132166162fa63282e8f59
Krishna2182 <kdasari2002@gmail.com>
2021-07-09 14:05:58 -0400

    Download GENIE data from Synapse

---
+++ code/synapse_API.py
@@ -0,0 +1,8 @@
+import synapseclient
+syn = synapseclient.Synapse()
+username = input('Synapse username: ')
+password = input('Synapse password: ')
+syn.login(username, password)
+entity = syn.get("syn24179660", downloadLocation="data/genie_9")
+entity = syn.get('syn24179661', downloadLocation="data/genie_9")
+entity = syn.get('syn24179664', downloadLocation="data/genie_9")
