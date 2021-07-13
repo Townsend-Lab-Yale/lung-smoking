0eff3230b18d15780dcaa74e1200c05ab46ae3a1
Jorge A. Alfaro-Murillo <jorge@bandersnatch.local>
2021-07-13 13:41:45 -0400

    lifover.py: Set location_data as variable

--- code/liftover.py
+++ code/liftover.py
@@ -1,14 +1,29 @@
 import lift_coords
+## The package lift_coords requires to have the LiftOver command-line
+## program. Obtain it from https://genome-store.ucsc.edu/
 import pandas as pd
 import os
+
+
+if '__file__' not in globals():
+    __file__ = '.'
+
+location_data = os.path.abspath(
+    os.path.join(os.path.dirname(__file__),
+                 "../"
+                 "data/"))
+"""Location of directory that contains data for the model."""
+
 
 g38hg38 = ['luad_tcga','luad_fm-ad']
 g37hg38 = ['luad_oncosg_2020','luad_broad','luad_mskcc_2015','luad_tsp','lung_msk_2017','nsclc_pd1_msk_2018']#,'nsclc_tracerx_2017']
 #tracerX doesn't work for some reason
 hg38 = ['genie_9']
 
-for i in g37hg38:
-    df = pd.read_csv('data/' + i + '/data_mutations_extended.txt', sep="\t", comment="#")
+for db in g37hg38:
+    df = pd.read_csv(os.path.join(location_data, db, "data_mutations_extended.txt"),
+                     sep="\t",
+                     comment="#")
     df2, failed = lift_coords.lift_over(df, 'grch37', 'grch38')#, keep_orig=True)
     #df3, failed = lift_coords.lift_over(df2, 'hg19', 'hg38')
     print(failed.shape)
