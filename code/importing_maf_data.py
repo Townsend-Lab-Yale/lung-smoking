ce57ba9980946e21328321fe930fd830bb60ee56
Jorge A. Alfaro Murillo <jorge.alfaro-murillo@yale.edu>
2021-07-13 15:12:58 -0400

    importing_maf_data.py: Tidy up code; improve docs

--- code/importing_maf_data.py
+++ code/importing_maf_data.py
@@ -19,70 +19,57 @@ location_output = os.path.abspath(
 """Location of directory that contains output for the model."""
 
 
-default_db_file_name = os.path.join(location_data, "tcga.luad.maf.txt")
-"""Default file name to use as data source."""
+data_sets_directories = {
+    'TSP':'luad_tsp',
+    'OncoSG':'luad_oncosg_2020',
+    'MSK2015':'luad_mskcc_2015',
+    'Broad':'luad_broad',
+    'MSK2018':'nsclc_pd1_msk_2018',
+    'MSK2017':'lung_msk_2017',
+    'TCGA':'luad_tcga',
+    'TracerX':'nsclc_tracerx_2017',
+    'Genie':'genie_9',
+    'FM-AD':'luad_fm-ad'}
+"""Location of the directories that contain each data set in the
+`location_data' directory.
+
+"""
+
+data_sets_sample_id_col_names ={key:'case_id' if key in ['TCGA', 'FM-AD']
+                                else None
+                                for key in data_sets_directories.keys()}
 
 
-def db_full_name(db_file):
-    """Return full path file name of `db_file'.
-
-    :typed db_file: str
-    :param db_file: Name of the MAF file containing the data. It
-        should be located in the :const:`location_data`. Data files
-        names available thus far are:
-
-            - 'cca_ihc.txt' alias 'ihc'
-            - 'melanoma.maf.txt' alias 'melanoma'
-            - 'rectal_adenocarcinoma.maf.txt' alias 'rectal'
-            - 'prostate_adenocarcinoma.maf.txt' alias 'prostate'
-            - 'ucec.maf.txt' alias 'ucec'
-            - 'tcga.luad.maf.txt' alias 'luad'
-            - 'tcga.lusc.maf.txt' alias 'lusc'
-
-    """
-    db_file_name = db_file.lower()
-    '''
-    if db_file_name in "ihc":
-        db_file_name = "cca_ihc.txt"
-    if db_file_name == "melanoma":
-        db_file_name = 'melanoma.maf.txt'
-    if db_file_name == "rectal":
-        db_file_name = 'rectal_adenocarcinoma.maf.txt'
-    if db_file_name == "prostate":
-        db_file_name = 'prostate_adenocarcinoma.maf.txt'
-    if db_file_name == "ucec":
-        db_file_name = 'ucec.maf.txt'
-    if db_file_name == 'luad':
-        db_file_name = 'tcga.luad.maf.txt'
-    if db_file_name == 'lusc':
-        db_file_name = 'tcga.lusc.maf.txt'
-
-    if "/" not in db_file_name:
-    '''
-    db_file_name = os.path.join(location_data, db_file_name)
-
-    return db_file_name
+data_sets_full_maf_file_names = {
+    db:os.path.join(location_data,
+                    directory,
+                    "data_mutations_extended.txt")
+    for db, directory in data_sets_directories.items()}
 
 
-def filter_db_by_mutation(db=default_db_file_name,
-                          tumor_col_name=None,
-                          sample_id_col_name=None,
-                          clear_silent=True):
-    """Build a data frame with sample IDs and mutations.
 
-    :type db: str or list
-    :param db: Name of the MAF file (or files if a db is a list)
-        containing the data. It should be located in the
-        :const:`location_data`. Data files names available thus far
-        are:
 
-            - 'cca_ihc.txt' alias 'ihc'
-            - 'melanoma.maf.txt' alias 'melanoma'
-            - 'rectal_adenocarcinoma.maf.txt' alias 'rectal'
-            - 'prostate_adenocarcinoma.maf.txt' alias 'prostate'
-            - 'ucec.maf.txt' alias 'ucec'
-            - 'tcga.luad.maf.txt' alias 'luad'
-            - 'tcga.lusc.maf.txt' alias 'lusc'
+def import_maf_data(db,
+                    tumor_col_name=None,
+                    sample_id_col_name=None,
+                    clear_silent=True):
+    """Build a data frame with relevant information including sample IDs
+and mutations.
+
+    :typed db: str or list
+    :param db: Name of the data set or list containing multiple data
+        set names. Data files names available thus far are:
+
+            -'TSP'
+            -'OncoSG'
+            -'MSK2015'
+            -'Broad'
+            -'MSK2018'
+            -'MSK2017'
+            -'TCGA'
+            -'TracerX'
+            -'Genie'
+            -'FM-AD'
 
     :type tumor_col_name: str
     :param tumor_col_name: Name of the column in the data that
@@ -104,15 +91,17 @@ def filter_db_by_mutation(db=default_db_file_name,
     """
 
     if isinstance(db, list):
-        db_full = [db_full_name(x) for x in db]
-        dfs = [pd.read_csv(x, sep="\t", comment="#")
-               for x in db_full]
-        dfs = [df.assign(Source=db_name)
-               for df, db_name in zip(dfs, db)]
+        dfs = [pd.read_csv(data_sets_full_maf_file_names[x],
+                           sep="\t",
+                           comment="#")
+               for x in db]
+        dfs = [df.assign(Source=x)
+               for df, x in zip(dfs, db)]
         data = pd.concat(dfs, ignore_index=True)
     else:
-        db_full = db_full_name(db)
-        data = pd.read_csv(db_full, sep="\t", comment="#")
+        data = pd.read_csv(data_sets_full_maf_file_names[db],
+                           sep="\t",
+                           comment="#")
         data = data.assign(Source=db)
 
     if clear_silent:
@@ -155,42 +144,32 @@ def filter_db_by_mutation(db=default_db_file_name,
 
     return data
 
-files = {
-    'TSP':['luad_tsp/data_mutations_extended.txt',''],
-    'OncoSG':['luad_oncosg_2020/data_mutations_extended.txt',''],
-    'MSK2015':['luad_mskcc_2015/data_mutations_extended.txt',''],
-    'Broad':['luad_broad/data_mutations_extended.txt',''],
-    'MSK2018':['nsclc_pd1_msk_2018/data_mutations_extended.txt',''],
-    'MSK2017':['lung_msk_2017/data_mutations_extended.txt',''],
-    'TCGA':['luad_tcga/data_mutations_extended.txt','case_id'],
-    'TracerX':['nsclc_tracerx_2017/data_mutations_extended.txt',''],
-    'Genie':['genie_9/data_mutations_extended.txt',''],
-    'FM-AD':['luad_FM-AD/data_mutations_extended.txt','case_id']
-}
 
-for key, value in files.items():
-    if value[1] == '':
-        files[key].append(filter_db_by_mutation(db = value[0]))
-    else:
-        files[key].append(filter_db_by_mutation(db = value[0], sample_id_col_name=value[1]))
+dfs = {
+    db:import_maf_data(
+        db,
+        sample_id_col_name=data_sets_sample_id_col_names[db])
+    for db in data_sets_directories.keys()}
 
-files['Broad'][2] = files['Broad'][2][files['Broad'][2]['Sample ID'] != 'LU-A08-43']
 
-files['MSK2017'][2] = files['MSK2017'][2][~files['MSK2017'][2]['Sample ID'].isin(metastatic_sample_ids_2017)]
-files['MSK2017'][2] = files['MSK2017'][2][~files['MSK2017'][2]['Sample ID'].isin(multi_sample_ids_2017)]
-files['MSK2017'][2] = files['MSK2017'][2][~files['MSK2017'][2]['Sample ID'].isin(dup_sample_ids_1718)]
+dfs['Broad'] = dfs['Broad'][dfs['Broad']['Sample ID'] != 'LU-A08-43']
 
-files['TracerX'][2] = files['TracerX'][2][~files['TracerX'][2]['Sample ID'].isin(metastatic_sample_ids_tracer)]
-files['TracerX'][2] = files['TracerX'][2][files['TracerX'][2]['Sample ID'].isin(keep_tracer_samples)]
+dfs['MSK2017'] = dfs['MSK2017'][~dfs['MSK2017']['Sample ID'].isin(metastatic_sample_ids_2017)]
+dfs['MSK2017'] = dfs['MSK2017'][~dfs['MSK2017']['Sample ID'].isin(multi_sample_ids_2017)]
+dfs['MSK2017'] = dfs['MSK2017'][~dfs['MSK2017']['Sample ID'].isin(dup_sample_ids_1718)]
 
-files['Genie'][2] = files['Genie'][2][~files['Genie'][2]['Sample ID'].isin(genie_non_luad_id)]
-files['Genie'][2] = files['Genie'][2][~files['Genie'][2]['Sample ID'].isin(metastatic_sample_ids_genie)]
-files['Genie'][2] = files['Genie'][2][~files['Genie'][2]['Sample ID'].isin(multi_sample_ids_genie)]
-files['Genie'][2] = files['Genie'][2][~files['Genie'][2]['Sample ID'].isin(dup_sample_ids_gen18)]
-files['Genie'][2] = files['Genie'][2][~files['Genie'][2]['Sample ID'].isin(dup_sample_ids_gen17)]
+dfs['TracerX'] = dfs['TracerX'][~dfs['TracerX']['Sample ID'].isin(metastatic_sample_ids_tracer)]
+dfs['TracerX'] = dfs['TracerX'][dfs['TracerX']['Sample ID'].isin(keep_tracer_samples)]
 
-files['FM-AD'][2] = files['FM-AD'][2][~files['FM-AD'][2]['Sample ID'].isin(fmad_non_luad)]
-files['FM-AD'][2] = files['FM-AD'][2][~files['FM-AD'][2]['Sample ID'].isin(fmad_non_primary)]
+dfs['Genie'] = dfs['Genie'][~dfs['Genie']['Sample ID'].isin(genie_non_luad_id)]
+dfs['Genie'] = dfs['Genie'][~dfs['Genie']['Sample ID'].isin(metastatic_sample_ids_genie)]
+dfs['Genie'] = dfs['Genie'][~dfs['Genie']['Sample ID'].isin(multi_sample_ids_genie)]
+dfs['Genie'] = dfs['Genie'][~dfs['Genie']['Sample ID'].isin(dup_sample_ids_gen18)]
+dfs['Genie'] = dfs['Genie'][~dfs['Genie']['Sample ID'].isin(dup_sample_ids_gen17)]
 
-final_file = pd.concat([value[2] for value in files.values()])
-final_file.to_csv(os.path.join(location_output, 'merged_luad_maf.txt'))
+dfs['FM-AD'] = dfs['FM-AD'][~dfs['FM-AD']['Sample ID'].isin(fmad_non_luad)]
+dfs['FM-AD'] = data_sets['FM-AD'][~data_sets['FM-AD']['Sample ID'].isin(fmad_non_primary)]
+
+
+final_df = pd.concat([df for df in dfs.values()])
+final_df.to_csv(os.path.join(location_output, 'merged_luad_maf.txt'))
