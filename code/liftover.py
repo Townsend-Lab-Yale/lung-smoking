b32892a0cc2aad9dd00630e7674d6a575d90c440
Jorge A. Alfaro Murillo <jorge.alfaro-murillo@yale.edu>
2021-07-13 16:46:44 -0400

    Start translation of coordinates
    TracerX and Genie are still not working.

--- code/liftover.py
+++ code/liftover.py
@@ -1,31 +1,46 @@
-import lift_coords
+import pandas as pd
+from lift_coords import lift_over
 ## The package lift_coords requires to have the LiftOver command-line
 ## program. Obtain it from https://genome-store.ucsc.edu/
-import pandas as pd
-import os
+
+from locations import full_maf_file_names
+
+destination_build = 'grch38'
 
 
-if '__file__' not in globals():
-    __file__ = '.'
+builds = {'TCGA':'grch38',
+          'FM-AD':'grch38',
+          'OncoSG':'grch37',
+          'Broad':'grch37',
+          'MSK2015':'grch37',
+          'TSP':'grch37',
+          'MSK2017':'grch37',
+          'MSK2018':'grch37',
+          # 'TracerX':'grch37',
+          # 'Genie':'hg38'
+          }
 
-location_data = os.path.abspath(
-    os.path.join(os.path.dirname(__file__),
-                 "../"
-                 "data/"))
-"""Location of directory that contains data for the model."""
+
+print("Importing original data frames...")
+original_dfs = {db:pd.read_csv(full_maf_file_names[db],
+                               sep="\t",
+                               comment="#")
+                for db in builds.keys()}
+print("...done.")
+print("")
 
 
-g38hg38 = ['luad_tcga','luad_fm-ad']
-g37hg38 = ['luad_oncosg_2020','luad_broad','luad_mskcc_2015','luad_tsp','lung_msk_2017','nsclc_pd1_msk_2018']#,'nsclc_tracerx_2017']
-#tracerX doesn't work for some reason
-hg38 = ['genie_9']
+lifted_dfs = {}
+failed_indices = {db:[] for db in builds.keys()}
 
-for db in g37hg38:
-    df = pd.read_csv(os.path.join(location_data, db, "data_mutations_extended.txt"),
-                     sep="\t",
-                     comment="#")
-    df2, failed = lift_coords.lift_over(df, 'grch37', 'grch38')#, keep_orig=True)
-    #df3, failed = lift_coords.lift_over(df2, 'hg19', 'hg38')
-    print(failed.shape)
-    print(df2.head())
-    #print(df3)
+
+for db, build in builds.items():
+    if build != destination_build:
+        print(f"Lifting coordinates of {db} from {build} to {destination_build}...")
+        lifted_dfs[db], failed_indices[db] = lift_over(original_dfs[db],
+                                                       build,
+                                                       destination_build)
+        print("...done.")
+        print("")
+    else:
+        lifted_dfs[db] = original_dfs[db]
