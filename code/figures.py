ac2458a0a42d0de22cc14a3662d4a4c3639e8578
Jorge A. Alfaro-Murillo <jorge@bandersnatch.local>
2021-08-26 18:08:09 -0400

    figures.py: Plot gammas

--- code/figures.py
+++ code/figures.py
@@ -1,23 +1,41 @@
+import os
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
+from locations import location_data
 from locations import fluxes_mles_file_name
 from locations import fluxes_cis_file_name
 from locations import location_figures
 
+
 lambdas_mles = np.load(fluxes_mles_file_name, allow_pickle=True).item()
 lambdas_cis = np.load(fluxes_cis_file_name, allow_pickle=True).item()
 
-# mus = {gene:0.1 for gene in lambdas_mles.keys()} ## need to load mut_rates here
-# gammas_mles = {gene:{x_y:mle/mus[gene]
-#                      for x_y, mle in lambdas.items()}
-#                for gene, lambdas in lambdas_mles.items()}
-# gammas_cis = {gene:{x_y:ci/mus[gene]
-#                     for x_y, ci in lambdas.items()}
-#               for gene, lambdas in lambdas_cis.items()}
+
+mut_rates_keys = ["pan-data", "exome", "smoking", "nonsmoking"]
+mut_rates = {key:pd.read_csv(os.path.join(location_data,
+                                          f'{key}_mutation_rates.txt'),
+                        index_col=0)
+             for key in mut_rates_keys}
 
 
-def plot_estimates_ordered(all_mles, all_cis, plot_name=None):
+def compute_gammas_mles(key):
+    gammas_mles = {gene:{x_y:mle/float(mut_rates[key].loc[gene])
+                         for x_y, mle in lambdas.items()}
+                   for gene, lambdas in lambdas_mles.items()}
+    return gammas_mles
+
+
+def compute_gammas_cis(key):
+    gammas_cis = {gene:{x_y:[ci[0]/float(mut_rates[key].loc[gene]),
+                             ci[1]/float(mut_rates[key].loc[gene])]
+                        for x_y, ci in lambdas.items()}
+                  for gene, lambdas in lambdas_cis.items()}
+    return gammas_cis
+
+
+def plot_estimates_ordered(all_mles, all_cis, xaxis_args=None, plot_name=None):
     mles_110_to_111 = {gene:mles[((1, 1, 0), (1, 1, 1))]
                        for gene, mles in all_mles.items()}
     cis_110_to_111 = {gene:cis[((1, 1, 0), (1, 1, 1))]
@@ -41,32 +59,62 @@ def plot_estimates_ordered(all_mles, all_cis, plot_name=None):
     ax.set_ylim(-0.5, len(genes_ordered)-0.5)
     ax.set_ylabel("gene")
 
-    x_lim = 0.4
-    x_tick_each = 0.1
-    x_minor_tick_each = 0.05
-    ax.set_xticks(np.arange(0, x_lim+x_tick_each, x_tick_each))
-    ax.set_xticks(np.arange(0, x_lim+x_minor_tick_each, x_minor_tick_each),
-                  minor=True)
-    ax.set_xlim(0, x_lim)
-    ax.set_xlabel("Flux from TP53+KRAS to TP53+KRAS+'gene'")
+    if xaxis_args is None:
+       xaxis_args = {}
+
+    if 'tick_each' in xaxis_args.keys():
+        ax.set_xticks(
+            np.arange(0,
+                      xaxis_args['lim'],
+                      xaxis_args['tick_each']))
+
+    if 'tick_minor_each' in xaxis_args.keys():
+        ax.set_xticks(
+            np.arange(0,
+                      xaxis_args['lim']+xaxis_args['tick_minor_each'],
+                      xaxis_args['tick_minor_each']),
+            minor=True)
+
+    if 'tick_labels' in xaxis_args.keys():
+        ax.set_xticklabels(xaxis_args['tick_labels'])
+
+    if 'lim' in xaxis_args.keys():
+        ax.set_xlim(0, xaxis_args['lim'])
+
+
+    if 'label' in xaxis_args.keys():
+        ax.set_xlabel(xaxis_args['label'])
 
 
     plt.tight_layout()
     if plot_name is None:
-        fig.savefig(os.path.join(location_figures,
-                                 "estimates_ordered.png"))
-    else:
-        fig.savefig(os.path.join(location_figures,
-                                 f"{plot_name}.png"))
+        plot_name = "estimates_ordered.png"
+
+    fig.savefig(os.path.join(location_figures,
+                             f"{plot_name}"))
     plt.close('all')
-    return mles_110_to_111
+    return cis_110_to_111
 
 
 def plot_all():
     plot_estimates_ordered(lambdas_mles, lambdas_cis,
-                           plot_name='fluxes_ordered.png')
-    # plot_estimates_ordered(gammas_mles, gammas_cis,
-    #                        plot_name='selection_coeffs_ordered.png')
+                           plot_name='fluxes_ordered',
+                           xaxis_args={'lim':0.42,
+                                       'label':"Flux from TP53+KRAS to TP53+KRAS+'gene'",
+                                       'tick_each':0.1,
+                                       'tick_minor_each':0.01})
+    for key in mut_rates_keys:
+        plot_estimates_ordered(
+            compute_gammas_mles(key),
+            compute_gammas_cis(key),
+            plot_name=f"selection_coeffs_ordered_{key}.png",
+            xaxis_args={'lim':270*10**3,
+                        'label':("Selection from "
+                                 "TP53+KRAS to TP53+KRAS+'gene' "
+                                 "(thousands)"),
+                        'tick_each':50*10**3,
+                        'tick_minor_each':10*10**3,
+                        'tick_labels':["0", "50K", "100K", "150K", "200K", "250K"]})
 
 
 if __name__ == "__figures__":
