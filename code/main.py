2430f883e11aff7893062791112c68ed414b824c
Krishna2182 <kdasari2002@gmail.com>
2021-09-03 21:42:25 -0400

    Summarize all gene panels in one file

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from pandas._libs.missing import NA
 from count_combinations import compute_samples
 from cancer_epistasis import numbers_positive_lambdas
 from cancer_epistasis import estimate_lambdas
@@ -14,9 +15,22 @@ from locations import fluxes_cis_file_name
 ## We fix M=3 so
 numbers_positive_lambdas = numbers_positive_lambdas[3]
 
-db = pd.read_csv(merged_maf_clinical_file_name)
+genie_panels_used = pd.read_csv('../gene_panels/genie_panels_used.txt').set_index('Sample Identifier').to_dict()
+msk2017_panels_used = pd.read_csv('../gene_panels/msk2017_panels_used.txt').set_index('Sample Identifier').to_dict()
+msk2018_panels_used = pd.read_csv('../gene_panels/msk2018_panels_used.txt').set_index('Sample Identifier').to_dict()
+
+db = pd.read_csv('../output/merged_luad_maf.txt')
 db = db[db['Variant_Classification'] != 'Silent']
 db = db[~pd.isnull(db['Mutation'])]
+#for now in here, but can be cut and pasted into the importing_maf_data file
+db.loc[db['Source'] == 'Genie','Panel'] = db['Sample ID'].map(genie_panels_used['Sequence Assay ID'])
+db.loc[db['Source'] == 'MSK2017','Panel'] = db['Sample ID'].map(msk2017_panels_used['Gene Panel'])
+db.loc[db['Source'] == 'MSK2018','Panel'] = db['Sample ID'].map(msk2018_panels_used['Gene Panel'])
+db.loc[db['Source'] == 'TSP', 'Panel'] = 'TSP'
+db.loc[db['Source'] == 'FM-AD', 'Panel'] = 'FoundationOne'
+
+all_panel_genes = pd.read_csv('../gene_panels/all_panel_genes.txt')
+included_panels = pd.unique(all_panel_genes['SEQ_ASSAY_ID'])
 
 #ranked_gene_list = ['TP53', 'KRAS', 'EGFR', 'STK11', 'ATM', 'NF1', 'BRAF', 'PIK3CA', 'CDKN2A', 'ERBB4', 'APC', 'NTRK3', 'EPHA3', 'MET', 'EPHA5', 'RB1', 'ERBB2', 'KDR', 'ALK', 'SMAD4', 'GNAS', 'PDGFRA', 'PTEN', 'FLT1', 'CTNNB1', 'BRCA2', 'NOTCH1', 'NTRK1', 'ATR', 'NOTCH2', 'FLT4', 'RET', 'PIK3CG', 'NOTCH4', 'BRCA1', 'KIT', 'TSC2', 'SMO', 'NOTCH3', 'PIK3C2G', 'FLT3', 'JAK3', 'DDR2', 'EPHB1', 'PDGFRB', 'JAK2', 'NTRK2', 'BARD1', 'MAP3K1', 'FBXW7', 'WT1', 'TSHR', 'CSF1R', 'ABL1', 'FGFR4', 'ERBB3', 'CBL', 'IGF1R', 'IRS2', 'MSH6', 'FGFR1', 'RICTOR', 'FGFR2', 'NRAS', 'TSC1', 'PIK3R1', 'MSH2', 'CHEK2', 'BTK', 'PIK3C3', 'RUNX1', 'PTPN11', 'CDH1', 'BAP1', 'AKT3', 'ARAF', 'FGFR3', 'BCL6', 'AXL', 'ERG', 'RAF1', 'JAK1', 'NF2', 'SMAD2', 'TGFBR2', 'MPL', 'MLH1', 'MAP2K1', 'MAP3K13', 'MYCN', 'MDM4', 'MAP2K4', 'MEN1', 'CCNE1', 'IKBKE', 'AKT1', 'FGF3', 'CCND2', 'PIK3R2', 'ETV6', 'SYK', 'CHEK1', 'SRC', 'AKT2', 'MYC', 'GATA1', 'MDM2', 'REL', 'VHL', 'HRAS', 'MAP2K2', 'CDKN1B', 'CDK8', 'CDK6', 'PRKAR1A', 'GSK3B', 'SUFU', 'CCND1', 'FGF4', 'AURKB', 'JUN', 'CCND3', 'CDKN2C', 'CDK4', 'BCL2', 'CRKL', 'CDKN2B']
 ranked_gene_list = ['TP53', 'KRAS', 'EGFR', 'STK11', 'ATM', 'NF1', 'BRAF', 'PIK3CA', 'ERBB4', 'APC', 'NTRK3', 'EPHA3', 'MET', 'EPHA5', 'RB1', 'ERBB2', 'KDR', 'ALK', 'SMAD4', 'GNAS', 'PDGFRA', 'PTEN', 'FLT1', 'CTNNB1', 'BRCA2', 'NOTCH1', 'NTRK1', 'ATR', 'NOTCH2', 'FLT4', 'RET', 'PIK3CG', 'NOTCH4', 'BRCA1', 'KIT', 'TSC2', 'SMO', 'NOTCH3', 'PIK3C2G', 'FLT3', 'JAK3', 'DDR2', 'EPHB1', 'PDGFRB', 'JAK2', 'NTRK2', 'BARD1', 'MAP3K1', 'FBXW7', 'WT1', 'TSHR', 'CSF1R', 'ABL1', 'FGFR4', 'ERBB3', 'CBL', 'IGF1R', 'IRS2', 'MSH6', 'FGFR1', 'RICTOR', 'FGFR2', 'NRAS', 'TSC1', 'PIK3R1', 'MSH2', 'CHEK2', 'BTK', 'PIK3C3', 'RUNX1', 'PTPN11', 'CDH1', 'BAP1', 'AKT3', 'ARAF', 'FGFR3', 'BCL6', 'AXL', 'ERG', 'RAF1', 'JAK1', 'NF2', 'SMAD2', 'TGFBR2', 'MPL', 'MLH1', 'MAP2K1', 'MAP3K13', 'MYCN', 'MDM4', 'MAP2K4', 'MEN1', 'CCNE1', 'IKBKE', 'AKT1', 'FGF3', 'CCND2', 'PIK3R2', 'ETV6', 'SYK', 'CHEK1', 'SRC', 'AKT2', 'MYC', 'GATA1', 'MDM2', 'REL', 'VHL', 'HRAS', 'MAP2K2', 'CDKN1B', 'CDK8', 'CDK6', 'PRKAR1A', 'GSK3B', 'SUFU', 'CCND1', 'FGF4', 'AURKB', 'JUN', 'CCND3', 'CDKN2C', 'CDK4', 'BCL2', 'CRKL', 'CDKN2B']
@@ -67,7 +81,13 @@ def main():
         print(f"Running model with third gene {gene} "
               f"(gene number {i}/{len(ranked_gene_list[2:])})")
         genes = ['TP53', 'KRAS', gene]
-        samples = compute_samples(db, mutations=genes)
+
+        panels_to_remove = []
+        for panel in included_panels:
+            if 'ABL1' not in all_panel_genes[all_panel_genes['SEQ_ASSAY_ID'] == panel]['Hugo_Symbol'].tolist():
+                panels_to_remove.append(panel)
+        subsetted_db = db[~db['Panel'].isin(panels_to_remove)]
+        samples = compute_samples(subsetted_db, mutations=genes)
 
         print("Estimating fluxes...")
         mle = lambdas_from_samples(samples)
