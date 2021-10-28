15ea95efa93420eeca5226c1b17ebdcc21a68f71
Krishna2182 <kdasari2002@gmail.com>
2021-10-28 01:32:06 -0400

    Calculate confidence intervals for selection intensities

--- code/selection_intensities.R
+++ code/selection_intensities.R
@@ -4,29 +4,31 @@ library(data.table)
 #PAN-DATA
 cesa_total = load_cesa('../data/pan-dataset_samples_cesa.rds')
 cesa_total = ces_variant(cesa_total)
-
-gene_selection = split(cesa_total$selection$selection.1$selection_intensity, cesa_total$selection$selection.1$gene)
-gene_selection = sapply(gene_selection, function(x){sum(x)})
-fwrite(setDT(as.data.frame(gene_selection), keep.rownames = TRUE), '../output/pan_data_selections_no_epistasis.txt')
 save_cesa(cesa_total, '../data/pan-dataset_samples_cesa.rds')
+
+pan_data_si = cesa_total$selection$selection.1[, .(variant_name, gene, selection_intensity, ci_low_95, ci_high_95)]
+pan_data_si = pan_data_si[,.(selection_intensity = sum(selection_intensity), ci_low_95 = sum(ci_low_95), ci_high_95 =sum(ci_high_95)),by=.(gene)]
+fwrite(pan_data_si, '../output/pan_data_selections_no_epistasis.txt')
 
 #SMOKING
 cesa_smoking = load_cesa('../data/smoking_samples_cesa.rds')
 cesa_smoking = ces_variant(cesa_smoking)
-
-gene_selection = split(cesa_smoking$selection$selection.1$selection_intensity, cesa_smoking$selection$selection.1$gene)
-gene_selection = sapply(gene_selection, function(x){sum(x)})
-fwrite(setDT(as.data.frame(gene_selection), keep.rownames = TRUE), '../output/smoking_selections_no_epistasis.txt')
 save_cesa(cesa_smoking, '../data/smoking_samples_cesa.rds')
+
+smoking_si = cesa_smoking$selection$selection.1[, .(variant_name, gene, selection_intensity, ci_low_95, ci_high_95)]
+smoking_si = smoking_si[,.(selection_intensity = sum(selection_intensity), ci_low_95 = sum(ci_low_95), ci_high_95 =sum(ci_high_95)),by=.(gene)]
+fwrite(smoking_si, '../output/smoking_selections_no_epistasis.txt')
+
 
 #NONSMOKING
 cesa_nonsmoking = load_cesa('../data/nonsmoking_samples_cesa.rds')
 cesa_nonsmoking = ces_variant(cesa_nonsmoking)
-
-gene_selection = split(cesa_nonsmoking$selection$selection.1$selection_intensity, cesa_nonsmoking$selection$selection.1$gene)
-gene_selection = sapply(gene_selection, function(x){sum(x)})
-fwrite(setDT(as.data.frame(gene_selection), keep.rownames = TRUE), '../output/nonsmoking_selections_no_epistasis.txt')
 save_cesa(cesa_nonsmoking, '../data/nonsmoking_samples_cesa.rds')
+
+nonsmoking_si = cesa_nonsmoking$selection$selection.1[, .(variant_name, gene, selection_intensity, ci_low_95, ci_high_95)]
+nonsmoking_si = nonsmoking_si[,.(selection_intensity = sum(selection_intensity), ci_low_95 = sum(ci_low_95), ci_high_95 =sum(ci_high_95)),by=.(gene)]
+fwrite(nonsmoking_si, '../output/nonsmoking_selections_no_epistasis.txt')
+
 
 
 
@@ -34,3 +36,6 @@ save_cesa(cesa_nonsmoking, '../data/nonsmoking_samples_cesa.rds')
 ratioed_selections = as.data.frame(gene_selection / gene_selection['KRAS'])
 ratioed_selections = setDT(ratioed_selections, keep.rownames = TRUE)[] 
 fwrite(ratioed_selections, '../temp/ratioed_selections.txt')
+
+
+
