e4039d84eff0f70fdcf0ba2456ba899bb9e15933
Krishna2182 <kdasari2002@gmail.com>
2022-05-20 09:57:49 -0500

    Updating Synapse API

--- code/synapse_API.py
+++ code/synapse_API.py
@@ -1,9 +1,19 @@
 import synapseclient
+import os
+from locations import location_data
+
 syn = synapseclient.Synapse()
 username = input('Synapse username: ')
 password = input('Synapse password: ')
 syn.login(username, password)
-entity = syn.get("syn24179660", downloadLocation="../data/genie_9")
-entity = syn.get('syn24179661', downloadLocation="../data/genie_9")
-entity = syn.get('syn24179664', downloadLocation="../data/genie_9")
-entity = syn.get('syn24179674', downloadLocation="../data/genie_9")
+
+
+# patient clinical data
+entity = syn.get("syn24179660", downloadLocation= os.path.join(location_data, "genie_9"))
+# sample data
+entity = syn.get('syn24179661', downloadLocation= os.path.join(location_data, "genie_9"))
+# MAF file
+entity = syn.get('syn24179664', downloadLocation= os.path.join(location_data, "genie_9"))
+# genomic information (genome coverage of TGS)
+entity = syn.get('syn24179674', downloadLocation= os.path.join(location_data, "genie_9"))
+
