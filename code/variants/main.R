6250159d4dde36570b1eeac72f2212a78685767a
Krishna2182 <kdasari2002@gmail.com>
2022-06-17 16:12:36 -0400

    Fix parameter for construct_maf and remove deprecated code

--- code/variants/main.R
+++ code/variants/main.R
@@ -41,39 +41,8 @@ fwrite(gene_df, paste0(location_output,"variant_based_mutation_rates.txt"))
 #' Produce MAF for cancer epistasis analysis
 #' Output location: 'data/cesR_maf_for_epistasis_analysis.txt'
 preloaded_maf = rbind(rbindlist(Broad_maf), FMAD_maf, rbindlist(Genie_maf), MSK2015_maf, rbindlist(MSK2017_maf), rbindlist(MSK2018_maf), OncoSG_maf, TCGA_maf, TracerX_maf, TSP_maf, fill = T)
-final_maf = construct_maf(cesa, maf_file, preloaded_maf, save_results = T)
+final_maf = construct_maf(cesa, maf_file, preloaded_maf, save_results)
 
 #' Additionally, create genes per sample table for new compute_samples functionality
 #' Output location: 'output/genes_per_sample.txt'
 samples_genes = produce_genes_per_sample(filtered_maf, gene_df$gene, save_results)
-
-
-
-
-
-
-#~~~deprecated~~~#
-
-# EXAMPLE USAGE WITH CADD
-#print('loading data')
-#cadd_table = fread('../../temp/cadd_scores.tsv')
-#cesa = load_cesa('../../data/pan_data_cesa_for_cancer_epistasis.rds')
-#maf_file = fread("../../output/merged_luad_maf.txt")
-
-#print('creating maf file with variant classification column and CADD scores')
-#unsure if I need to preload each dataset separately
-##maf_file = preload_maf(maf_file, refset = ces.refset.hg19, chain_file = "../../data/hg38ToHg19.over.chain", sample_col = 'Sample ID', keep_extra_columns = T)
-##var_classification = maf_file[,.(variant_id, Variant_Classification)]
-##var_classification = var_classification[!duplicated(var_classification$variant_id)]
-# MAF to be used in cancer epistasis analysis
-##cesa = integrate_cadd_scores(cesa, cadd_table)
-##cesa@maf = merge(cesa$maf, var_classification, by='variant_id', all.x = T)
-
-#gene_df = fread('../../data/genes_list.txt', header = F)
-#colnames(gene_df) = 'gene'
-#gene_df[,gene := toupper(gene)]
-
-#print('computing gene mutation rates')
-#genome_trinucleotide_mutation_proportions = compute_genome_trinucleotide_mut_proportions(cesa)
-#gene_df[, mutation_rates := lapply(gene, function(x) sum(variant_mutation_rate(x, cesa, genome_trinucleotide_mutation_proportions)))]
-#fwrite(gene_df, '../../output/variant_based_mutation_rates.txt')
