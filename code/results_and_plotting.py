3be8665c103bf9cf2436bfc7fc5244181c0d1e7a
Krishna2182 <kdasari2002@gmail.com>
2022-07-25 11:28:57 -0400

    Pathway analysis

--- code/results_and_plotting.py
+++ code/results_and_plotting.py
@@ -2,14 +2,17 @@ import numpy as np
 import pandas as pd
 import os
 
+from itertools import chain
 
 from filter_data import key_filtered_dbs
 from filter_data import filter_samples_for_genes
 
 from count_combinations import updated_compute_samples
-from main import are_all_fluxes_computable
+from main import at_least_000_to_001_and_110_to_111
 from cancer_epistasis import asymp_CI_lambdas, compute_CI_gamma, convert_lambdas_to_dict, estimate_lambdas, compute_gammas
 from theory import build_S_as_array
+from theory import order_pos_lambdas
+
 from locations import full_mutation_rate_file_names
 
 location_results = '../to_delete'
@@ -68,7 +71,9 @@ def at_least_000_to_010_and_001_to_011(samples):
     return np.all(samples[[0,1]] > 0)
 
 '''
-TODO: check if necessary sample counts are fulfilled based on user input
+TODO: 
+- check if necessary sample counts are fulfilled based on user input
+- make bound changes for 4+ genes
 '''
 def produce_results_for_one_gene_set(genes, dataset):
     '''
@@ -79,20 +84,58 @@ def produce_results_for_one_gene_set(genes, dataset):
     results = {}
 
     print("Counting samples on each mutation combination for " + ', '.join(genes) + "...")
-    samples = updated_compute_samples(db, mutations=genes)
+    samples = updated_compute_samples(db, mutations=genes, print_info=True)
     results["samples"] = convert_samples_to_dict(samples)
-    print("...done")
     print("")
 
     print("Estimating fluxes MLE...")
-    mle = estimate_lambdas(samples, draws=1)
-    results["lambdas"] = convert_lambdas_to_dict(mle)
+    bounds = 1
+    print(f"Bounds for fluxes: {bounds}")
+    mle = estimate_lambdas(samples, draws=1,
+                            upper_bound_prior=bounds,
+                            kwargs={'return_raw':True})
+
+    bound_changes = 0
+    while mle[1].fun < -1e+20:
+        if len(genes) != 3:
+            raise Exception("Lambdas incalculable at bounds=1, suitable bound changes for more or less than are yet to be implemented")
+        if bound_changes == 4:
+            return "incomputable"
+        print(f"Algorithm did not converge changing bounds...")
+        bound_changes += 1
+        bounds = np.array(
+            [1/(2**bound_changes),  # (0, 0, 0) -> (0, 0, 1)
+                1,                     # (0, 0, 0) -> (0, 1, 0)
+                1,                     # (0, 0, 1) -> (0, 1, 1)
+                1/(2**bound_changes),  # (0, 1, 0) -> (0, 1, 1)
+                1,                     # (0, 0, 0) -> (1, 0, 0)
+                1,                     # (0, 0, 1) -> (1, 0, 1)
+                1/(2**bound_changes),  # (1, 0, 0) -> (1, 0, 1)
+                1,                     # (0, 1, 0) -> (1, 1, 0)
+                1,                     # (1, 0, 0) -> (1, 1, 0)
+                1,                     # (0, 1, 1) -> (1, 1, 1)
+                1,                     # (1, 0, 1) -> (1, 1, 1)
+                1/(2**bound_changes)]) # (1, 1, 0) -> (1, 1, 1)
+        print(f"Proposed bounds: {bounds}")
+        mle = estimate_lambdas(samples, draws=1,
+                                upper_bound_prior=bounds,
+                            kwargs={'return_raw':True})
+
+    if len(genes) == 3:
+        S_3 = build_S_as_array(3)
+        states_with_zero = [tuple(x) for x in S_3[samples == 0]]
+        indices_with_zero = [order_pos_lambdas(S_3).index((x, tuple(y))) for x, y in order_pos_lambdas(S_3) if x in states_with_zero]
+        for x in indices_with_zero: mle[0]['lambdas'][x] = np.nan
+    else:
+        print("Warning: Combinations with 0 samples cannot have the fluxes from them discarded at this point for n != 3")
+
+    results["lambdas"] = convert_lambdas_to_dict(mle[0])
     print("...done")
     print("")
 
     print("Computing fluxes confidence intervals (CI)...")
     results["lambdas_cis"] = convert_lambdas_to_dict(
-        asymp_CI_lambdas(mle['lambdas'],
+        asymp_CI_lambdas(mle[0]['lambdas'],
                          samples,
                          print_progress=False))
     print("...done")
@@ -115,22 +158,27 @@ def produce_results_for_one_gene_set(genes, dataset):
     return results
 
 def produce_results(gene_combinations, dataset, save=True):
+    '''
+    Takes NESTED LISTS for gene_combinations where within the list is each set of genes to be tested
+    '''
     results = {'_'.join(gene_set): produce_results_for_one_gene_set(gene_set, dataset) for gene_set in gene_combinations}
     if save:
         print("")
         print("Saving results...")
         location_dataset = os.path.join(location_results, dataset)
-        if not os.path.isdir(location_dataset):
-            os.mkdir(location_dataset)
         for gene_set, results_dict in results.items():
-            location_gene_set = os.path.join(location_results, dataset, gene_set)
-            if not os.path.isdir(location_gene_set):
-                os.mkdir(location_gene_set)
+            if results_dict != "incomputable":
+                if not os.path.isdir(location_dataset):
+                    os.mkdir(location_dataset)
+                location_gene_set = os.path.join(location_dataset, gene_set)
+                if not os.path.isdir(location_gene_set):
+                    os.mkdir(location_gene_set)
 
-            for key, value in results_dict.items():
-                np.save(os.path.join(location_gene_set,
-                                    f'{key}.npy'), value)
+                for key, value in results_dict.items():
+                    np.save(os.path.join(location_gene_set,
+                                        f'{key}.npy'), value)
         print("...done")
+    return results
 
 
 def produce_results_for_pathway_analysis(samples, genes_and_pathways, dataset, save=True):
@@ -142,17 +190,57 @@ def produce_results_for_pathway_analysis(samples, genes_and_pathways, dataset, s
     results = {}
 
     print("Estimating fluxes MLE...")
-    mle = estimate_lambdas(samples, draws=1)
-    results["lambdas"] = convert_lambdas_to_dict(mle)
+    bounds = 1
+    print(f"Bounds for fluxes: {bounds}")
+    mle = estimate_lambdas(samples, draws=1,
+                            upper_bound_prior=bounds,
+                            kwargs={'return_raw':True})
+
+    bound_changes = 0
+    while mle[1].fun < -1e+20:
+        if len(genes_and_pathways) != 3:
+            raise Exception("Lambdas incalculable at bounds=1, suitable bound changes for more or less than are yet to be implemented")
+        if bound_changes == 4:
+            return "incomputable"
+        print(f"Algorithm did not converge changing bounds...")
+        bound_changes += 1
+        bounds = np.array(
+            [1/(2**bound_changes),  # (0, 0, 0) -> (0, 0, 1)
+                1,                     # (0, 0, 0) -> (0, 1, 0)
+                1,                     # (0, 0, 1) -> (0, 1, 1)
+                1/(2**bound_changes),  # (0, 1, 0) -> (0, 1, 1)
+                1,                     # (0, 0, 0) -> (1, 0, 0)
+                1,                     # (0, 0, 1) -> (1, 0, 1)
+                1/(2**bound_changes),  # (1, 0, 0) -> (1, 0, 1)
+                1,                     # (0, 1, 0) -> (1, 1, 0)
+                1,                     # (1, 0, 0) -> (1, 1, 0)
+                1,                     # (0, 1, 1) -> (1, 1, 1)
+                1,                     # (1, 0, 1) -> (1, 1, 1)
+                1/(2**bound_changes)]) # (1, 1, 0) -> (1, 1, 1)
+        print(f"Proposed bounds: {bounds}")
+        mle = estimate_lambdas(samples, draws=1,
+                                upper_bound_prior=bounds,
+                            kwargs={'return_raw':True})
+
+    if len(genes_and_pathways) == 3:
+        S_3 = build_S_as_array(3)
+        states_with_zero = [tuple(x) for x in S_3[samples == 0]]
+        indices_with_zero = [order_pos_lambdas(S_3).index((x, tuple(y))) for x, y in order_pos_lambdas(S_3) if x in states_with_zero]
+        for x in indices_with_zero: mle[0]['lambdas'][x] = np.nan
+    else:
+        print("Combinations with 0 samples cannot have the fluxes from them discarded at this point for n != 3")
+
+    results["lambdas"] = convert_lambdas_to_dict(mle[0])
     print("...done")
     print("")
 
     print("Computing fluxes confidence intervals (CI)...")
     results["lambdas_cis"] = convert_lambdas_to_dict(
-        asymp_CI_lambdas(mle['lambdas'],
-                        samples,
-                        print_progress=False))
+        asymp_CI_lambdas(mle[0]['lambdas'],
+                         samples,
+                         print_progress=False))
     print("...done")
+
 
     print("Importing mutation rates...")
     if 'plus' in dataset:
@@ -248,8 +336,25 @@ np.save(os.path.join(location_results, key,
 
 produce_results_for_pathway_analysis(samples, mutations, key)
 '''
+'''
+EGFR_pathway_genes = ['BRAF','MEK','ERK','MAPK','MAP2K1','PIK3CA','PTEN','AKT1','NFKB','RAS','RAF','RAC','TSC1','TSC2','NF1','JAK1','JAK2','SHC','SOS','GRB2','STAT','MYC','FOXO3A','IRS1','IRS2','PDK','AMPK1','STK11']
+RAS_pathway_genes = pd.read_csv('~/Downloads/PID_RAS_PATHWAY.v7.5.1.tsv', sep = '\t').iloc[18,1].split(',')
+PI3K_AKT_pathway_genes = pd.read_csv('~/Downloads/PID_PI3KCI_AKT_PATHWAY.v7.5.1.tsv', sep = '\t').iloc[18,1].split(',')
+MTOR_pathway_genes = pd.read_csv('~/Downloads/PID_MTOR_4PATHWAY.v7.5.1.tsv', sep = '\t').iloc[18,1].split(',')
 
+EGFR_pathway_genes = list(set(EGFR_pathway_genes + RAS_pathway_genes + PI3K_AKT_pathway_genes + MTOR_pathway_genes))
+#set(EGFR_pathway_genes) - set(RAS_pathway_genes + PI3K_AKT_pathway_genes + MTOR_pathway_genes)
+EGFR_pathway_genes.pop(EGFR_pathway_genes.index('KRAS'))
+
+from locations import gene_list_file
+gene_list = list(pd.read_csv(gene_list_file, header=None)[0])
+gene_list = [gene.upper() for gene in gene_list]
+gene_list = gene_list[:103]
+
+EGFR_pathway_genes = [gene for gene in EGFR_pathway_genes if gene in gene_list]
+
+for key in ['pan_data','smoking_plus','nonsmoking_plus']:
+    produce_results([['KRAS','EGFR',gene] for gene in EGFR_pathway_genes if gene != 'CCNE1'], key)
 '''
 for key in ['pan_data','smoking_plus','nonsmoking_plus']:
-    produce_results([['TP53','KRAS',gene] for gene in ['STK11','ARID1A']], key)
-'''
+    produce_results([['TP53','KRAS','STK11','KEAP1']], key)
