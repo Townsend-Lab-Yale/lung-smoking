41efbbb0cf3e8ae8a8d6c73512fa6c64132768f3
Krishna2182 <kdasari2002@gmail.com>
2023-06-02 11:31:56 -0500

    Remove old commented code

--- code/merge_MAF_clinical.py
+++ code/merge_MAF_clinical.py
@@ -13,18 +13,3 @@ maf_clinical = pd.merge(maf, clinical, on="Sample ID", how = 'outer')
 maf_clinical = maf_clinical.drop(columns=['Unnamed: 0_x','Unnamed: 0_y'])
 
 maf_clinical.to_csv(merged_maf_clinical_file_name)
-
-'''Below are checks to ensure that the columns contain expected values.'''
-'''
-print(maf_clinical['Chromosome'].unique())
-#not sure how to check these three columns
-#print (maf_clinical[pd.to_numeric(maf_clinical['Start_Position'], errors='coerce').isnull()])
-#print (maf_clinical[pd.to_numeric(maf_clinical['Progression Free Survival (months)'], errors='coerce').isnull()])
-#print (maf_clinical[pd.to_numeric(maf_clinical['Overall Survival (months)'], errors='coerce').isnull()])
-print(maf_clinical['Variant_Classification'].unique())
-print(maf_clinical['Smoker'].unique())
-print(maf_clinical['Stage'].unique())
-print(maf_clinical['Treatment'].unique())
-print(maf_clinical['Metastatic'].unique())
-print(maf_clinical['Vital Status'].unique())
-'''
