89364eb04d1d2f923aa755a44312a1e1771c6a9a
Krishna2182 <kdasari2002@gmail.com>
2024-02-07 14:20:16 -0500

    Update variant-rate calculation method and modularize main.R

--- code/variants/main.R
+++ code/variants/main.R
@@ -28,38 +28,69 @@ source('create_cesa_for_epistasis.R')
 gene_df = fread(paste0(location_data, "genes_list.txt"), header = F)
 colnames(gene_df) = 'gene'
 gene_df[,gene := toupper(gene)]
-#' For now, only calculating mutation rates for the limited selection of genes that we are calculating fluxes for
-#' gene_df = gene_df[1:103,]
+#gene_df = gene_df[1:10,]
 
 #' Calculate trinucleotide mutation proportions across the whole genome in preparation for calculating variant mutation rates
+
+cesa = load_cesa(paste0(location_data,"pan_data_cesa_for_cancer_epistasis.rds"))
+smoking_samples = fread(paste0(location_data, 'smoking_sample_ids.txt'), header=F)[[1]]
+nonsmoking_samples = fread(paste0(location_data, 'nonsmoking_sample_ids.txt'), header=F)[[1]]
+panel_smoking_samples = fread(paste0(location_data, 'panel_smoking_sample_ids.txt'), header=F)[[1]]
+panel_nonsmoking_samples = fread(paste0(location_data, 'panel_nonsmoking_sample_ids.txt'), header=F)[[1]]
+
 genome_trinucleotide_mutation_proportions = genome_trinuc_mut_proportions(cesa$maf)
 smoking_genome_trinucleotide_mutation_proportions = genome_trinuc_mut_proportions(cesa$maf[Unique_Patient_Identifier %in%
                                                                                              c(smoking_samples, panel_smoking_samples)])
 nonsmoking_genome_trinucleotide_mutation_proportions = genome_trinuc_mut_proportions(cesa$maf[Unique_Patient_Identifier %in%
                                                                                              c(nonsmoking_samples, panel_nonsmoking_samples)])
 
+#' Store dataframe of all non-silent variants present within the gene across all samples (so that the same
+#'   set of variants are used to calculate the gene mutation rate for all genes)
+cesa_maf = copy(cesa$maf)
+cesa_maf = cesa_maf[lengths(genes) == 1 & !is.na(genes) & is.na(top_gene), top_gene := as.character(genes)]
+preloaded_maf = fread(paste0(location_data, 'all_preloaded_mafs.txt'))
+# filtering out silent variants
+variants_per_gene = merge(cesa_maf, preloaded_maf[is.na(problem) & Variant_Classification != 'Silent', 
+                              .(Unique_Patient_Identifier, variant_id)], 
+                          all=F)
+# excluding all non-SNVs
+variants_per_gene = variants_per_gene[top_gene %in% gene_df$gene & variant_type == 'snv' & !is.na(variant_id), .(top_gene, variant_id)]
+# excluding non-recurrent variants
+recurrent_variants_per_gene = variants_per_gene[, .N, by=.(top_gene, variant_id)][N>1]
+
 #' Calculate variant level mutation rates
 #' Then calculate the gene level mutation rate as the sum of all the variant level mutation rates within the gene
-gene_df[, pan_data_mutation_rates := lapply(gene, function(x) sum(variant_mutation_rate(x,
-                                                                                        cesa,
-                                                                                        genome_trinucleotide_mutation_proportions,
-                                                                                        samples = cesa$samples$Unique_Patient_Identifier)))]
-gene_df[, smoking_mutation_rates := lapply(gene, function(x) sum(variant_mutation_rate(x,
-                                                                                       cesa_smoking_w_panel,
-                                                                                       smoking_genome_trinucleotide_mutation_proportions,
-                                                                                       samples = c(smoking_samples, panel_smoking_samples))))]
-gene_df[, nonsmoking_mutation_rates := lapply(gene, function(x) sum(variant_mutation_rate(x,
-                                                                                          cesa_nonsmoking_w_panel,
-                                                                                          nonsmoking_genome_trinucleotide_mutation_proportions,
-                                                                                          samples = c(nonsmoking_samples, panel_nonsmoking_samples))))]
+
+pan_data_gene_rates = fread(paste0(location_output, 'pan_data_mutation_rates.txt'))
+gene_df[, pan_data_mutation_rates := 
+              sapply(gene, function(x)
+                gene_mutation_rate(x,
+                                   pan_data_gene_rates[gene==x, rate_grp_1],
+                                   genome_trinucleotide_mutation_proportions,
+                                   variants = recurrent_variants_per_gene[top_gene == x, variant_id]))]
+smoking_w_panel_gene_rates = fread(paste0(location_output, 'smoking_w_panel_mutation_rates.txt'))
+gene_df[, smoking_mutation_rates := 
+            sapply(gene, function(x) 
+                gene_mutation_rate(x,
+                                   smoking_w_panel_gene_rates[gene==x, rate_grp_1],
+                                   smoking_genome_trinucleotide_mutation_proportions,
+                                   variants = recurrent_variants_per_gene[top_gene == x, variant_id]))]
+
+nonsmoking_w_panel_gene_rates = fread(paste0(location_output, 'nonsmoking_w_panel_mutation_rates.txt'))
+gene_df[, nonsmoking_mutation_rates := 
+            sapply(gene, function(x) 
+                gene_mutation_rate(x,
+                                   nonsmoking_w_panel_gene_rates[gene==x, rate_grp_1],
+                                   nonsmoking_genome_trinucleotide_mutation_proportions,
+                                   variants = recurrent_variants_per_gene[top_gene == x, variant_id]))]
+
 
 fwrite(gene_df, paste0(location_output,"variant_based_mutation_rates.txt"))
 
-#' Produce MAF for cancer epistasis analysis
-#' Output location: 'data/cesR_maf_for_epistasis_analysis.txt'
-preloaded_maf = rbind(rbindlist(Broad_maf), FMAD_maf, rbindlist(Genie_maf), MSK2015_maf, rbindlist(MSK2017_maf), rbindlist(MSK2018_maf), OncoSG_maf, TCGA_maf, TracerX_maf, TSP_maf, NCI_maf,fill = T)
-# preloaded_maf = rbind(rbindlist(Broad_maf), FMAD_maf, MSK2015_maf, rbindlist(MSK2017_maf), rbindlist(MSK2018_maf), OncoSG_maf, TCGA_maf, TracerX_maf, TSP_maf, NCI_maf,fill = T)
-final_maf = construct_maf(cesa, maf_file, preloaded_maf, save_results)
+maf_file = read.csv(paste0(location_output,"merged_luad_maf.txt"))
+colnames(maf_file)[2] = 'Tumor_Sample_Barcode'; colnames(maf_file)[7] <- 'Tumor_Allele'
+
+final_maf = construct_maf(cesa$maf, maf_file, preloaded_maf, save_results)
 
 #' Additionally, create genes per sample table for new compute_samples functionality
 #' Output location: 'output/genes_per_sample.txt'
--- code/variants/variant_mutation_rate.R
+++ code/variants/variant_mutation_rate.R
@@ -11,33 +11,37 @@ library(stringr)
 # find #times that context is observed
 # var_mut_rate = gene_rate * freq / #times-context-observed
 
-variant_mutation_rate = function(gene, cesa, trinuc_proportion_matrix, samples=NA){
+variant_mutation_rate = function(gene, gene_syn_mutation_rate, trinuc_proportion_matrix, variants=NULL, maf_df=NULL, samples=NULL){
   gene_x = gene
-  print(gene_x)
+  # print(gene_x)
+  
   # certain variants have an NA variant_id, but none of these are in genes in our 1290 genes of interest.
-  maf = cesa$maf
   
-  if(any(is.na(samples))){stop('Please enter a list of samples. If you want to include all samples, use cesa$samples$Unique_Patient_Identifier')}
-  
-  variants = unique(cesa$maf[Unique_Patient_Identifier %in% samples & top_gene == gene_x & 
+  if(is.null(variants)){
+    print('No variant list provided. Using only the variants observed within the gene from the MAF dataframe provided.')
+    if(is.null(maf_df)){stop("MAF dataframe must be provided if variant list is not provided")}
+    if(is.null(samples)){stop("List of samples must be provided if variant list is not provided.
+                              If you want to include all samples, use cesa$samples$Unique_Patient_Identifier")}
+    variants = unique(maf_df[Unique_Patient_Identifier %in% samples & top_gene == gene_x & 
                                variant_type == 'snv' & !is.na(variant_id), 
                              variant_id])
+    if(length(variants) == 0){warning('There are no variants present in the dataset corresponding to this gene'); return(NA)}
+  } else if(length(variants) == 0){warning('The list of variants provided is empty.'); return(NA)}
   
-  if(length(variants) == 0){return(NA)}
-
+  variants = unique(variants)
   
-  temp1 = str_split_fixed(variants, pattern = ':', n=2)
-  temp2 = str_split_fixed(temp1[,2], pattern= '_', n=2)
-  variants = as.data.table(cbind(temp1[,1],temp2))
+  tmp1 = str_split_fixed(variants, pattern = ':', n=2)
+  tmp2 = str_split_fixed(tmp1[,2], pattern= '_', n=2)
+  variants = as.data.table(cbind(tmp1[,1],tmp2))
   colnames(variants) = c('chr','pos','mut')
-  variants$pos = strtoi(variants$pos)
+  variants[,pos := strtoi(pos)]
   
   #get trinuc context for variants
   
   #' 
   #' ASSUMPTION: all sequences taken from POSITIVE strand
   #' 
-  variants$trinuc = as.vector(as.character(getSeq(BSgenome.Hsapiens.UCSC.hg19, names = str_c('chr', variants$chr), start = variants$pos-1, width=3, strand = '+')))
+  variants[, trinuc := as.vector(as.character(getSeq(BSgenome.Hsapiens.UCSC.hg19, names = str_c('chr', chr), start = pos-1, width=3, strand = '+')))]
   
   #reverse complementing variants as necessary
   middle = str_c(substr(variants$trinuc,2,2),collapse="")
@@ -48,25 +52,27 @@ variant_mutation_rate = function(gene, cesa, trinuc_proportion_matrix, samples=N
                                            as.character(reverseComplement(DNAStringSet(variants[revcomp_ind, substr(mut,3,3)])))))]
   
   #generating COSMIC type format for trinuc mutations
-  variants$trinuc_mut = paste0(substr(variants$trinuc,1,1),'[', variants$mut,']',substr(variants$trinuc,3,3))
+  variants[,trinuc_mut := paste0(substr(trinuc,1,1),'[', mut,']',substr(trinuc,3,3))]
   
   #gets trinuc composition for each variant
-  variants$trinuc_mut_prop = as.vector(trinuc_proportion_matrix[variants$trinuc_mut])
+  variants[,trinuc_mut_prop := as.vector(trinuc_proportion_matrix[trinuc_mut])]
   
-  if(colnames(cesa$gene_rates)[2] == "rate"){
-    variants$gene_rate = cesa$gene_rates[gene == gene_x, rate]
-  } else if(colnames(cesa$gene_rates)[2] == "rate_grp_1"){
-    variants$gene_rate = cesa$gene_rates[gene == gene_x, rate_grp_1]
-  } else{stop('Gene mutation rate column not recognized')}
+  variants[,gene_rate := gene_syn_mutation_rate]
   
+  tri_nt_contexts = table(variants$trinuc)
+  variants[, context_freq := tri_nt_contexts[trinuc]]
   
-  #' This only works with the singular table returned by compute tri-nt contexts, 
-  #' if we were to vectorize this, we would need to make it dataframe-workable
-  tri_nt_contexts = compute_trinucleotide_contexts(gene_x)
-  variants$context_freq = tri_nt_contexts[variants$trinuc]
+  #' #' This only works with the singular table returned by compute tri-nt contexts, 
+  #' #' if we were to vectorize this, we would need to make it dataframe-workable
+  #' tri_nt_contexts = compute_trinucleotide_contexts(gene_x)
+  #' variants[,context_freq := tri_nt_contexts[trinuc]] # should use data.table notation
   
   #calculate mutation rate for variants
   variants[,mut_rate := gene_rate * trinuc_mut_prop / context_freq]
   
   return(variants$mut_rate)
+}
+
+gene_mutation_rate = function(gene, gene_syn_mutation_rate, trinuc_proportion_matrix, variants=NULL, maf_df=NULL, samples=NULL){
+  return(sum(variant_mutation_rate(gene, gene_syn_mutation_rate, trinuc_proportion_matrix, variants, maf_df, samples)))
 }
