8a7d83f96249a5dfe1a12c7a3faaabc02a874a80
Krishna2182 <kdasari2002@gmail.com>
2024-02-15 16:56:38 -0500

    maf_construction.R: Add option to only consider recurrent variants

--- code/variants/maf_construction.R
+++ code/variants/maf_construction.R
@@ -3,18 +3,27 @@
 library(cancereffectsizeR)
 library(data.table)
 
-construct_maf = function(cesa_maf, original_maf, preloaded_maf, save_results = TRUE){
+construct_maf = function(cesa_maf, original_maf, preloaded_maf, recurrent_variants_only = FALSE, save_results = TRUE){
   #' Assigning a single gene to each variant
-  cesa_maf = cesa_maf[lengths(genes) == 1 & !is.na(genes) & is.na(top_gene), top_gene := genes]
+  cesa_maf = cesa_maf[lengths(genes) == 1 & !is.na(genes) & is.na(top_gene), top_gene := as.character(genes)]
   cesa_maf_variants = cesa_maf[variant_type == 'snv', .(Unique_Patient_Identifier, variant_id)]
   
-  preload_maf_variants = preloaded_maf[variant_type == 'snv' & is.na(problem) & Variant_Classification != 'Silent', .(Unique_Patient_Identifier, variant_id)]
+  preload_maf_variants = preloaded_maf[variant_type == 'snv' & Variant_Classification != 'Silent' & is.na(problem) & !is.na(variant_id), .(Unique_Patient_Identifier, variant_id)]
   
   #' Taking the intersection of these loaded and preloaded variants as the ones to keep
   variants_to_keep = merge(cesa_maf_variants, preload_maf_variants) 
   
-  #' Keeping desired variants and removing extraneous columns
-  filtered_maf = merge(cesa_maf, variants_to_keep, by = c('Unique_Patient_Identifier','variant_id'))
+  if(recurrent_variants_only){
+    #' Retain only variants that recurrently appear in the dataset
+    recurrent_variants_to_keep = variants_to_keep[variants_to_keep[, .N, by=.(variant_id)][N>1], on=.(variant_id)]
+    recurrent_variants_to_keep[, N:=NULL]
+    
+    #' Keeping desired variants and removing extraneous columns
+    filtered_maf = merge(cesa_maf, recurrent_variants_to_keep, by = c('Unique_Patient_Identifier','variant_id'))
+  } else{
+    filtered_maf = merge(cesa_maf, variants_to_keep, by = c('Unique_Patient_Identifier','variant_id'))
+  }
+  
   filtered_maf = filtered_maf[,-c('prelift_chr', 'prelift_start', 'liftover_strand_flip','variant_type', 'genes', 'top_consequence')]
 
   #' Adding source information to all of the TGS samples
@@ -27,7 +36,7 @@ construct_maf = function(cesa_maf, original_maf, preloaded_maf, save_results = T
   #' Renaming columns for consistency between Python and R
   colnames(filtered_maf) = c('Sample ID', 'Mutation', 'Chromosome', 'Start_Position', 'Reference_Allele', 'Tumor_Seq_Allele2', 'Gene', 'Source', 'Panel')
   
-  if(save_results == TRUE){
+  if(save_results){
     fwrite(filtered_maf, paste0(location_output, "cesR_maf_for_epistasis_analysis.txt"))
   }
   
