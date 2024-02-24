e9ea3ad7863e0a5c1f2c10eedba17e95b0b3ec6d
Krishna2182 <kdasari2002@gmail.com>
2024-02-24 16:52:10 -0500

    maf_construction.R: use only retained variants from main.R to construct MAF

--- code/variants/maf_construction.R
+++ code/variants/maf_construction.R
@@ -3,7 +3,7 @@
 library(cancereffectsizeR)
 library(data.table)
 
-construct_maf = function(cesa_maf, original_maf, preloaded_maf, recurrent_variants_only = FALSE, save_results = TRUE){
+construct_maf = function(cesa_maf, original_maf, preloaded_maf, variants_per_gene, save_results = TRUE){
   #' Assigning a single gene to each variant
   cesa_maf = cesa_maf[lengths(genes) == 1 & !is.na(genes) & is.na(top_gene), top_gene := as.character(genes)]
   cesa_maf_variants = cesa_maf[variant_type == 'snv', .(Unique_Patient_Identifier, variant_id)]
@@ -13,17 +13,11 @@ construct_maf = function(cesa_maf, original_maf, preloaded_maf, recurrent_varian
   #' Taking the intersection of these loaded and preloaded variants as the ones to keep
   variants_to_keep = merge(cesa_maf_variants, preload_maf_variants) 
   
-  if(recurrent_variants_only){
-    #' Retain only variants that recurrently appear in the dataset
-    recurrent_variants_to_keep = variants_to_keep[variants_to_keep[, .N, by=.(variant_id)][N>1], on=.(variant_id)]
-    recurrent_variants_to_keep[, N:=NULL]
-    
-    #' Keeping desired variants and removing extraneous columns
-    filtered_maf = merge(cesa_maf, recurrent_variants_to_keep, by = c('Unique_Patient_Identifier','variant_id'))
-  } else{
-    filtered_maf = merge(cesa_maf, variants_to_keep, by = c('Unique_Patient_Identifier','variant_id'))
-  }
+  #' #' Only retain variants that were not removed per conditions in main.R (recurrents_only, trim_oncogenes)
+  #' variants_to_keep = variants_to_keep[variant_id %in% variants_per_gene$variant_id]
   
+  #' Keeping desired variants in MAF and removing extraneous columns
+  filtered_maf = merge(cesa_maf, variants_to_keep, by = c('Unique_Patient_Identifier','variant_id'))
   filtered_maf = filtered_maf[,-c('prelift_chr', 'prelift_start', 'liftover_strand_flip','variant_type', 'genes', 'top_consequence')]
 
   #' Adding source information to all of the TGS samples
