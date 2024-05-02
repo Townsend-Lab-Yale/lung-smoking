71d91bad927a930f27d60f83a1eb853c3e07dfad
Krishna2182 <kdasari2002@gmail.com>
2024-05-02 14:38:38 -0400

    Load mutation rates from WES/WGS samples only

--- code/load_results.py
+++ code/load_results.py
@@ -32,27 +32,23 @@ def load_mutation_rates(key, method, extension=None):
         extended_location_output = os.path.join(location_output,extension)
     else: extended_location_output = location_output
 
+    if key[-4:] == 'plus':
+            accession_key = key[:-5]
+    else: accession_key = key
+
     if(method == "variant"):
         variant_based_mutation_rates = pd.read_csv(os.path.join(extended_location_output,
                                                         'variant_based_mutation_rates.txt'),
                                                         index_col=0)
-        variant_based_mutation_rates = variant_based_mutation_rates.to_dict()
-
-        if key[-4:] == 'plus':
-            accession_key = key[:-5]
-        else: accession_key = key
+        variant_based_mutation_rates = variant_based_mutation_rates.to_dict()        
 
         mus = variant_based_mutation_rates[accession_key]
         mus = {gene: mu for gene, mu in mus.items() if not np.isnan(mu)}
 
     elif(method == "cesR"):
-        if key[-4:] == 'plus':
-            final_part_key = 'w_panel'
-        else:
-            final_part_key = key[-4:]
         mus_df = pd.read_csv(
             os.path.join(extended_location_output,
-                        f"{key[:-4] + final_part_key}_mutation_rates.txt"),
+                        f"{accession_key}_mutation_rates.txt"),
             index_col='gene')
 
         mus = mus_df["rate_grp_1"].to_dict()
